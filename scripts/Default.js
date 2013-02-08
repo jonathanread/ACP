248300dc6f34dff68c6891058173c042a4fba457
unknown <jread@JREAD-LAPTOP.apterasoftware.com>
2013-02-08 08:04:09 -0500

    In we go

--- scripts/Default.js
+++ scripts/Default.js
@@ -10,4 +10,15 @@ $(document).ready(function(){
     if($(".wrapper .rotator").length){
         $(".wrapper .rotator").cycle({ cleartype: true, cleartypeNoBg: true, pause: true,height:"100%",width:"100%" });
     }
+})
+.bind("mobileinit", function () {
+    $(".wrapper .header .menu li").tap(function(){
+        if($(this).children(".subMenu").css("display") == "none"){
+            $(this).children(".subMenu").show();
+        }
+        else{
+            $(this).children(".subMenu").hide();
+        }
+    });
+    $("#mobileAlert #close").tap(function () { $("#mobileAlert").hide();});
 });
