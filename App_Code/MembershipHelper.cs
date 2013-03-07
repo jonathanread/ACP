0f789a6f4a09abb1756d7343f79790ddad7bf2af
unknown <jread@JREAD-LAPTOP.apterasoftware.com>
2013-03-07 15:16:20 -0500

    New contacts and stuff

---
+++ App_Code/MembershipHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using System.Web.Security;
+
+/// <summary>
+/// Summary description for MembershipHelper
+/// </summary>
+public class MembershipHelper
+{
+    private static MembershipUser member { get; set; }
+    
+    public MembershipHelper()
+	{
+       
+	}
+
+    public static bool CreateCustomer(string name, string email){
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email))
+        {
+            return false;
+        }
+        else
+        {
+           member = Membership.CreateUser(email, "password", email);
+        }
+
+        return true;
+    }
+
+    public static bool CreateCustomer(string name, string email, string phone,object address, string customerType, DateTime firstContact, DateTime mostRecentContact, string typeOfContact, bool recieveNewsletter){
+        try
+        {
+            if (member != null)
+            {
+
+            }
+            else
+            {
+                member = Membership.CreateUser(email, "password", email);
+                var profile = ProfileBase.Create(member.UserName);
+                profile[""] = "";
+                profile.Save();
+            }
+        }
+        catch (Exception)
+        {
+            
+            throw;
+        }
+
+        return true;
+    }
+}
--- scripts/Default.js
+++ scripts/Default.js
@@ -19,4 +19,73 @@ $(document).ready(function(){
     		pager: $(".rotatorPager")
     	});
     }
+
+    if ($("#contact").length) {
+    	$(".ReqErrorMsg").hide();
+    	$("#Phone").mask("(999) 999-9999? ext99999", { placeholder: "#" });
+    	$("#ContactForm").submit(function () {
+    		var formData = $(this).serialize().split("&");
+    		return validateFields(formData);
+    	});
+    }
 });
+
+function validateFields(formData)
+{
+	var valid = true;
+	var fields = [];
+	var emRegEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+	for (var i in formData) {
+		var field = formData[i].split("=")[0].replace("=", "");
+		fields.push(field);
+
+		
+
+		if (formData[i].split("=")[1].length == 0) {
+			valid = false;
+			$("#" + field).siblings(".ReqErrorMsg").show();
+		}
+		else {
+			$("#" + field).siblings(".ReqErrorMsg").hide();
+			if (field == "Email") {
+				if (!emRegEx.test($("#" + field).val())) {
+					valid = false;
+					$("#" + field).siblings(".ReqErrorMsg").html("Invalid Email").show();
+				}
+				else {
+					$("#" + field).siblings(".ReqErrorMsg").html("This field is required.").hide();
+				}
+			}
+		}
+		if (field == "RequestType") {
+			if (formData[i].split("=")[1] == "Please+Select") {
+				valid = false;
+				$("#" + field).siblings(".ReqErrorMsg").show();
+			}
+			else {
+				$("#" + field).siblings(".ReqErrorMsg").hide();
+			}
+		}
+	}
+
+	if (fields.indexOf("contactTime") == -1) {
+		if ($("#contactTime").length) {
+
+			$("#contactTime").siblings(".ReqErrorMsg").show();
+		}
+	} else {
+		$("#contactTime").siblings(".ReqErrorMsg").hide();
+	}
+
+	if (fields.indexOf("contactMethod") == -1) {
+		if ($("#contactMethod").length) {
+			valid = false;
+			$("#contactMethod").siblings(".ReqErrorMsg").show();
+		}
+	}
+	else {
+		$("#contactMethod").siblings(".ReqErrorMsg").hide();
+	}
+	$("#loader").hide();
+	return valid;
+}
